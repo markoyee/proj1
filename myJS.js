23f0b9dc372e267f5d89391fe45efb235849ce7b
Marek <markoyee@users.noreply.github.com>
2018-08-22 20:46:18 +0200

    dodane nowe warstwy

--- myJS.js
+++ myJS.js
@@ -1,29 +1,29 @@
 $(document).ready(function () {
     //wczytywanie mapy
 
-    
     //dane z OSM
-    var lyrOSM = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png');
-     
+    var lyrOSM = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png'),
     //warstwa z ortofotomapy geoportal
-    var lyrORTO = L.tileLayer.wms("http://mapy.geoportal.gov.pl/wss/service/img/guest/ORTO/MapServer/WMSServer", {layers: "Raster", format: 'image/png', transparent : 'true', version : '1.1.1'});
-    
+    lyrORTO = L.tileLayer.wms("http://mapy.geoportal.gov.pl/wss/service/img/guest/ORTO/MapServer/WMSServer", {layers: "Raster", format: 'image/png', transparent : 'true', version : '1.1.1'}),    //warstwa z ortofotomapy geoportal
+    lyrSozo = L.tileLayer.wms("http://mapy.geoportal.gov.pl/wss/service/img/guest/SOZO/MapServer/WMSServer", {layers: "Raster", format: 'image/png', transparent : 'true', version : '1.1.1'});     //wartstwa sozologiczna z geoportalu
+
+         
     //tworzenie mapy
-    var mymap = L.map('mymap', {center: [52.1, 21.0], zoom: 10, zoomControl: false, attributionControl: false, layers:[lyrOSM,lyrORTO]});
+    var mymap = L.map('mymap', {center: [52.1, 21.0], zoom: 10, zoomControl: false, attributionControl: false, layers:[lyrOSM,lyrORTO,lyrSozo]});
     
     mymap.addLayer(lyrOSM);
     
     
+    
     var baseMaps = {
         "Openstreetmap": lyrOSM,
-        "Ortofotomapa": lyrORTO          
+        "Ortofotomapa": lyrORTO,
+        "Mapa Sozologiczna": lyrSozo
+
       };
     
+    //polecenie dodania ikonki 
     L.control.layers(baseMaps).addTo(mymap);
-    
-    
-    
-    
     
     //Działa ale zamula w chuj bo geoportal jest mało wydajny
 
