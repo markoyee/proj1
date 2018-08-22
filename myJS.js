ce7cf971da0e44423c37c899cf47f9a015b0dfa8
Marek <markoyee@users.noreply.github.com>
2018-08-22 19:47:28 +0200

    działa
    usunięto błędnie działający kawałek kodu dodający 2gą warstwę

--- myJS.js
+++ myJS.js
@@ -3,9 +3,9 @@ $(document).ready(function () {
     var mymap = L.map('mymap', {center: [52.1, 21.0], zoom: 10, zoomControl: false, attributionControl: false});
     var lyrOSM = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png');
     mymap.addLayer(lyrOSM);
+
     
-    var mymap2 = L.map('odp', {center: [52.1, 21.0], zoom: 10, zoomControl: false, attributionControl: false});
-    mymap2.addLayer(lyrOSM);
+    
     //tworze zmienna w postacie mojej ikonki.
     var myicon=L.icon({
         iconUrl: 'img/platoon.png',
