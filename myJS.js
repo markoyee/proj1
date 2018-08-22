70b7fc6e5aff3349478c046c4b7cbc09f9b807dd
Marek <markoyee@users.noreply.github.com>
2018-08-22 19:44:02 +0200

    wybor warstw
    dodano panel wyboru warstw,
    podłączono jako podkład mapy dane z geoportalu.

--- myJS.js
+++ myJS.js
@@ -1,9 +1,31 @@
 $(document).ready(function () {
     //wczytywanie mapy
-    var mymap = L.map('mymap', {center: [52.1, 21.0], zoom: 10, zoomControl: false, attributionControl: false});
+
+    
+    //dane z OSM
     var lyrOSM = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png');
+     
+    //warstwa z ortofotomapy geoportal
+    var lyrORTO = L.tileLayer.wms("http://mapy.geoportal.gov.pl/wss/service/img/guest/ORTO/MapServer/WMSServer", {layers: "Raster", format: 'image/png', transparent : 'true', version : '1.1.1'});
+    
+    //tworzenie mapy
+    var mymap = L.map('mymap', {center: [52.1, 21.0], zoom: 10, zoomControl: false, attributionControl: false, layers:[lyrOSM,lyrORTO]});
+    
     mymap.addLayer(lyrOSM);
     
+    
+    var baseMaps = {
+        "Openstreetmap": lyrOSM,
+        "Ortofotomapa": lyrORTO          
+      };
+    
+    L.control.layers(baseMaps).addTo(mymap);
+    
+    
+    
+    
+    
+    //Działa ale zamula w chuj bo geoportal jest mało wydajny
 
     //tworze zmienna w postacie mojej ikonki.
     var myicon=L.icon({
