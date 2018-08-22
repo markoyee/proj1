ca6edcb0a6810261ec13257832d06037013396d5
Marek <markoyee@users.noreply.github.com>
2018-08-22 13:50:11 +0200

    delete additional maplayer
    second map layer is not working correctly

--- myJS.js
+++ myJS.js
@@ -4,8 +4,7 @@ $(document).ready(function () {
     var lyrOSM = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png');
     mymap.addLayer(lyrOSM);
     
-    var mymap2 = L.map('odp', {center: [52.1, 21.0], zoom: 10, zoomControl: false, attributionControl: false});
-    mymap2.addLayer(lyrOSM);
+
     //tworze zmienna w postacie mojej ikonki.
     var myicon=L.icon({
         iconUrl: 'img/platoon.png',
